a9b04062f0c21d7976cecb0e33c20fb57daffd16
ayesha-siddiqa20 <ayeshaahmeds222@gmail.com>
2022-11-17 14:06:46 -0500

    Added changes to num_values_trailing_leading_spaces v14

--- piperider_cli/profiler/profiler.py
+++ piperider_cli/profiler/profiler.py
@@ -614,6 +614,7 @@ class StringColumnProfiler(BaseColumnProfiler):
                 result = conn.execute(stmt).fetchone()    
                 result2 = session.query(func.count(cte.c.c).label("_num_values_with_trailing_leading_spaces").\
                     filter(or_(cte.c.c.like(" %"), cte.c.c.like("% ")))) # new code
+                print("this is the result ", result2)
                 _total, _non_nulls, _valids, _zero_length, _distinct, _avg, _min, _max, _variance = result
                 _num_values_with_trailing_leading_spaces = result2
                 _stddev = None
@@ -625,6 +626,7 @@ class StringColumnProfiler(BaseColumnProfiler):
                 # stmt2 = select(columns2)
                 result2 = session.query(func.count(cte.c.c).label("_num_values_with_trailing_leading_spaces").\
                     filter(or_(cte.c.c.like(" %"), cte.c.c.like("% ")))) # new code
+                print("this is the result ", result2)
                 result = conn.execute(stmt).fetchone()                                  
                 _total, _non_nulls, _valids, _zero_length, _distinct, _avg, _min, _max, _stddev = result
                 _num_values_with_trailing_leading_spaces = result2

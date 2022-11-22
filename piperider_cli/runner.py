77cbe6c22cf7d7d34f8220c49025f3a361f5f4dc
ayesha-siddiqa20 <ayeshaahmeds222@gmail.com>
2022-11-22 00:00:33 -0500

    fixed issue - mode list [null] v4

--- piperider_cli/runner.py
+++ piperider_cli/runner.py
@@ -25,7 +25,8 @@ from piperider_cli.configuration import Configuration
 from piperider_cli.datasource import DataSource
 from piperider_cli.exitcode import EC_ERR_TEST_FAILED
 from piperider_cli.filesystem import FileSystem
-from piperider_cli.profiler import Profiler, ProfilerEventHandler, value_of_mode
+from piperider_cli.profiler import Profiler, ProfilerEventHandler
+import piperider_cli.profiler
 
 class RunEventPayload:
 
@@ -668,7 +669,7 @@ class Runner():
         # console.print(run_result)
         # console.print(run_result["tables"][name_of_result_table]["columns"])
 
-        console.print(value_of_mode)
+        console.print(piperider_cli.profiler.value_of_mode)
 
         with open(output_file, 'w') as f:
             f.write(json.dumps(run_result["tables"][name_of_result_table]["columns"], separators=(',', ':')))
